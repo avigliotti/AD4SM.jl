36178028537e5219ba7304915f35edf0dc2143a9
andrea <andrea.vigliotti@gmail.com>
2019-09-15 18:20:19 +0200

    test

--- src/AD4SM.jl
+++ src/AD4SM.jl
@@ -2,4 +2,11 @@ module AD4SM
 
 greet() = print("Hello World!")
 
+include("adiff.jl")
+include("materials.jl")
+include("elements.jl")
+
+# using .adiff, .Materials, .Elements 
+# export adiff, Materials, Elements 
+
 end # module
