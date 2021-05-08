a4a6fe1f4994ff9acbd2483620e070e04b619a10
andrea <andrea.vigliotti@gmail.com>
2021-05-08 11:42:50 +0200

    added copy for elements

--- src/elements.jl
+++ src/elements.jl
@@ -64,20 +64,20 @@ Elems   = Union{Rod, Beam, C2D, C3D, CAS}
 CElems  = Union{C2D, C3D, CAS}
 CartEls = Union{C2D, C3D}
 # copy
-copy(elem::C2D{M} where M) = C2D{M}(copy(elem.nodes), elem.Nx, elem.Ny, elem.wgt, elem.V, elem.mat)
-copy(elem::C3D{M} where M) = C3D{M}(copy(elem.nodes), elem.Nx, elem.Ny, elem.Nz, elem.wgt, elem.V, elem.mat)
-copy(elem::CAS{M} where M) = CAS{M}(copy(elem.nodes), elem.N0, elem.Nx, elem.Ny, elem.X0, elem.wgt, elem.V, elem.mat)
+copy(elem::C2D{M}) where M = C2D{M}(copy(elem.nodes), elem.Nx, elem.Ny, elem.wgt, elem.V, elem.mat)
+copy(elem::C3D{M}) where M = C3D{M}(copy(elem.nodes), elem.Nx, elem.Ny, elem.Nz, elem.wgt, elem.V, elem.mat)
+copy(elem::CAS{M}) where M = CAS{M}(copy(elem.nodes), elem.N0, elem.Nx, elem.Ny, elem.X0, elem.wgt, elem.V, elem.mat)
 copy(elem::Rod)            = Rod(copy(elem.nodes), elem.r0, elem.l0, elem.A, elem.mat)
 copy(elem::Beam)           = Beam(copy(elem.nodes), elem.r0, elem.L, elem.w, elem.lgwx, elem.lgwy, elem.mat)
 # clone
-function clone(elem::C2D{M} where M)
+function clone(elem::C2D{M}) where M
   nodes = copy(elem.nodes)
   Nx    = tuple([copy(x) for x in elem.Nx]...)
   Ny    = tuple([copy(x) for x in elem.Ny]...)
   
   C2D{M}(nodes, Nx, Ny, elem.wgt, elem.V, elem.mat)
 end
-function clone(elem::C3D{M} where M)
+function clone(elem::C3D{M}) where M
   nodes = copy(elem.nodes)
   Nx    = tuple([copy(x) for x in elem.Nx]...)
   Ny    = tuple([copy(x) for x in elem.Ny]...)
@@ -85,7 +85,7 @@ function clone(elem::C3D{M} where M)
   
   C3D{M}(nodes, Nx, Ny, Nz, elem.wgt, elem.V, elem.mat)
 end
-function clone(elem::CAS{M} where M)
+function clone(elem::CAS{M}) where M
   nodes = copy(elem.nodes)
   N0    = tuple([copy(x) for x in elem.N0]...)
   Nx    = tuple([copy(x) for x in elem.Nx]...)
